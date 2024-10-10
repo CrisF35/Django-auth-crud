3fff7601fd9989d9074e99790b28d89c78c14a88
CrisF35 <cristopheromg35@gmail.com>
2024-10-09 23:50:25 -0500

    XD

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:mysecretpassword@localhost:5433/tasks32',
+        default='postgresql://postgres:mysecretpassword@localhost:5433/tasks',
         conn_max_age=600
     )
 }
