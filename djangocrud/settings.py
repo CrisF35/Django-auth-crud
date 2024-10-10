a2b9195eb15f86bf915af5defc83d576dc699a98
CrisF35 <cristopheromg35@gmail.com>
2024-10-09 23:53:57 -0500

    que asco

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -84,13 +84,11 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:mysecretpassword@localhost:5433/tasks',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
